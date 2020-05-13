4bd86a98063c2aa81d71be42e443934d668f8f27
bucceroni <bucceroni@hotmail.com>
2020-05-13 16:16:54 -0300

    add capacitor storage

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,7 +7,10 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
   constructor(public photoService: PhotoService) { }
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
 
 }
